11bed9c8dc6ed156fe23bab6f65ea3d8bc2200dc
telanks <769967440@qq.com>
2017-09-25 00:04:43 +0800

    配置css 导入 配置 babel文件 ES6语法 react语法

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,6 +1,23 @@
 // Greeter.js
-module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent = "前端大佬!";
-    return greet;
-};
+//var config = require('./config.json');
+//module.exports = function() {
+//    var greet = document.createElement('div');
+//    greet.textContent = config.greetText;
+//    return greet;
+//};
+
+
+import React,{Component} from 'react'
+import config from './config.json'
+
+class Greeter extends Component{
+    render(){
+        return (
+            <div>
+                {config.greetText}
+            </div>
+        );
+    }
+}
+
+export default Greeter
