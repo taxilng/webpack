40aa991887b97e04928e5813b2c608af38e112ec
thinkpadE550 <769967440@qq.com>
2017-12-05 17:39:26 +0800

    放到header模块中

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
+        <header-view></header-view>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import headerView from './components/header.vue'
     export default {
-        name: 'app'
+        name: 'app',
+        components:{
+            headerView
+        }
     }
 </script>
 <style scoped>
